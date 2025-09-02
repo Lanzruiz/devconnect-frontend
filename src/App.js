1d8caf8d467d3cf98bd069a0b21a99b6ba5bb52b
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-09-02 19:04:34 +0800

    Implement onSubmit and onchange in Register and Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import { Routes, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <Fragment>
       <Navbar />
-      <Landing />
-    </div>
+
+      <Routes>
+        <Route exact path="/" Component={Landing} />
+        <Route exact path="/register" Component={Register} />
+        <Route exact path="/login" Component={Login} />
+      </Routes>
+    </Fragment>
   );
 };
 
---
+++ src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {};
+
+  return (
+    <Fragment>
+      <section className="container">
+        <div className="alert alert-danger">Invalid credentials</div>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Sign into Your Account
+        </p>
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              required
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Login;
