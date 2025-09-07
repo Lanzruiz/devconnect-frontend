5205256e483e1235460cb17e1def4e4cfbf9a396
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-09-07 14:21:30 +0800

    Implement the reducer of auth and action

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Alert from "../layout/Alert";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -21,32 +22,12 @@ const Register = ({ setAlert }) => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      setAlert("Password do not match", "danger", 2000);
+      setAlert("Password do not match", "danger", 3000);
     } else {
-      console.log("Success!");
-      //   const newUser = {
-      //     name,
-      //     email,
-      //     password,
-      //   };
-
-      //   try {
-      //     const config = {
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //       },
-      //     };
-
-      //     const body = JSON.stringify(newUser);
-
-      //     const res = await axios.post("/api/users", body, config);
-      //     console.log(res.data);
-      //   } catch (err) {
-      //     console.error(err.response.data);
-      //   }
+      register({ name, email, password });
     }
   };
-  //   console.log(formData);
+
   return (
     <section className="container">
       <Alert />
@@ -106,6 +87,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
--- src/reducers/alert.js
+++ src/reducers/alert.js
@@ -2,8 +2,8 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 const initialState = [];
 
 export default function (state = initialState, action) {
-  const { typle, payload } = action;
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
     case SET_ALERT:
       return [...state, payload];
     case REMOVE_ALERT:
